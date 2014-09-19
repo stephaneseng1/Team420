b87e93960871204d9b8d4584fff5538470c3fa2b
stephaneseng1 <stephane.seng1@collegeahuntsic.qc.ca>
2014-09-19 15:20:20 -0400

    DAO update

--- bibliotheque - 2/src/dao/DAO.java
+++ bibliotheque - 2/src/dao/DAO.java
@@ -1,6 +1,15 @@
 
 package dao;
 
-public class DAO {
-
+public class DAO extends Connection {
+	private Connection getConnection(){
+		return this.connection;
+	}
+	private void setConnection(Connection connection){
+		this.connection = connection;
+	}
+	
+	protected Connection getConnection(){
+		return getConnexion().getConnection();
+	}
 }
