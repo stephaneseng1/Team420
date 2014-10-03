66bd770bda1d2a061076919fdd8d4157e2037c36
tuananlor <Tuan-An.Lor.1@collegeahuntsic.qc.ca>
2014-10-03 16:59:26 -0400

    completer pretfacade et apporter des modifications avec IMembreDAO

--- bibliotheque - 4/src/ca/qc/collegeahuntsic/bibliotheque/dao/interfaces/IMembreDAO.java
+++ bibliotheque - 4/src/ca/qc/collegeahuntsic/bibliotheque/dao/interfaces/IMembreDAO.java
@@ -1,5 +1,20 @@
+
 package ca.qc.collegeahuntsic.bibliotheque.dao.interfaces;
 
-public class IMembreDAO {
-//TODO
+import java.util.List;
+import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
+import ca.qc.collegeahuntsic.bibliotheque.dto.MembreDTO;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.DAOException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidCriterionException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidHibernateSessionException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidSortByPropertyException;
+
+public interface IMembreDAO extends IReservationDAO {
+    List<MembreDTO> findByNom(Connexion connexion,
+        String nom,
+        String sortByPropertyName) throws InvalidHibernateSessionException,
+        InvalidCriterionException,
+        InvalidSortByPropertyException,
+        DAOException;
+
 }
--- bibliotheque - 4/src/ca/qc/collegeahuntsic/bibliotheque/facade/implementations/PretFacade.java
+++ bibliotheque - 4/src/ca/qc/collegeahuntsic/bibliotheque/facade/implementations/PretFacade.java
@@ -1,5 +1,70 @@
+
 package ca.qc.collegeahuntsic.bibliotheque.facade.implementations;
 
-public class PretFacade {
-//TODO
+import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
+import ca.qc.collegeahuntsic.bibliotheque.dto.PretDTO;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidCriterionException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidHibernateSessionException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidPrimaryKeyException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidPrimaryKeyRequestException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidSortByPropertyException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOClassException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dto.MissingDTOException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.facade.FacadeException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.facade.InvalidServiceException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.service.ExistingLoanException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.service.ExistingReservationException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.service.InvalidLoanLimitException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.service.MissingLoanException;
+import ca.qc.collegeahuntsic.bibliotheque.facade.interfaces.IPretFacade;
+import ca.qc.collegeahuntsic.bibliotheque.service.interfaces.IPretService;
+
+public class PretFacade extends Facade implements IPretFacade {
+
+    public PretFacade(IPretService pretService) throws InvalidServiceException {
+    }
+
+    @Override
+    public void commencer(Connexion connexion,
+        PretDTO pretDTO) throws InvalidHibernateSessionException,
+        InvalidDTOException,
+        InvalidPrimaryKeyException,
+        MissingDTOException,
+        InvalidCriterionException,
+        InvalidSortByPropertyException,
+        ExistingLoanException,
+        InvalidLoanLimitException,
+        ExistingReservationException,
+        InvalidDTOClassException,
+        InvalidPrimaryKeyRequestException,
+        FacadeException;
+
+    @Override
+    public void renouveler(Connexion connexion,
+        PretDTO pretDTO) throws InvalidHibernateSessionException,
+        InvalidDTOException,
+        InvalidPrimaryKeyException,
+        MissingDTOException,
+        InvalidCriterionException,
+        InvalidSortByPropertyException,
+        MissingLoanException,
+        ExistingLoanException,
+        ExistingReservationException,
+        InvalidDTOClassException,
+        FacadeException;
+
+    @Override
+    public void terminer(Connexion connexion,
+        PretDTO pretDTO) throws InvalidHibernateSessionException,
+        InvalidDTOException,
+        InvalidPrimaryKeyException,
+        MissingDTOException,
+        InvalidCriterionException,
+        InvalidSortByPropertyException,
+        MissingLoanException,
+        ExistingLoanException,
+        InvalidDTOClassException,
+        FacadeException;
+
 }
