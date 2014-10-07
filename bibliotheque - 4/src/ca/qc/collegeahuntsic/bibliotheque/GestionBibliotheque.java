f722bac8a133093b61dff107424f8f3242da808f
Fred <godstyle8@gmail.com>
2014-10-07 16:07:43 -0400

    Création GestionBibliotheque

---
+++ bibliotheque - 4/src/ca/qc/collegeahuntsic/bibliotheque/GestionBibliotheque.java
@@ -0,0 +1,15 @@
+package ca.qc.collegeahuntsic.bibliotheque;
+
+public class GestionBibliotheque {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new GestionBibliotheque();
+		
+	}
+	public GestionBibliotheque(){}
+
+}
