b7f6614dee66e189afd0c8b9ff8c9f1219f8348b
tuananlor <Tuan-An.Lor.1@collegeahuntsic.qc.ca>
2014-11-04 13:56:03 -0500

    generer les javadoc biliotheque - 6

---
+++ bibliotheque - 6/src/ca/qc/collegeahuntsic/bibliotheque/exception/util/package-info.java
@@ -0,0 +1,11 @@
+// Fichier package-info.java
+// Auteur : 201176542
+// Date de création : 2014-11-04
+/**
+ * Package BDCreateurException
+ *
+ * @author 201176542
+ */
+
+package ca.qc.collegeahuntsic.bibliotheque.exception.util;
+
